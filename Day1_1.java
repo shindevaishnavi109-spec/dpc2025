1935adb7af5bb30286d17f045b07ad31944fcc52
vaishnavi shinde <shindevaishnavi109@gmail.com>
2025-08-15 23:13:18 +0530

    Create Day1_1.java
    Day1 challenge

---
+++ Day1_1.java
@@ -0,0 +1,41 @@
+public class Sort012 {
+    public static void sortArray(int[] arr) {
+        int low = 0, mid = 0, high = arr.length - 1;
+
+        while (mid <= high) {
+            switch (arr[mid]) {
+                case 0:
+                    int temp0 = arr[low];
+                    arr[low] = arr[mid];
+                    arr[mid] = temp0;
+                    low++;
+                    mid++;
+                    break;
+
+                case 1:
+                    mid++;
+                    break;
+
+                case 2:
+                    int temp2 = arr[mid];
+                    arr[mid] = arr[high];
+                    arr[high] = temp2;
+                    high--;
+                    break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 2, 1, 0, 2, 1, 0};
+        
+        sortArray(arr);
+
+        // Print sorted array
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}   
+
+
